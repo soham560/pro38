f49f29d702e2105cbce1f0d6f2d46b1a738803f6
soham560 <77228631+soham560@users.noreply.github.com>
2021-08-16 12:51:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var canvas
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var player, player1, player2, player3, player4;
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
+}
